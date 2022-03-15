d5b1a2ee4e70caa2f4ba2e37a4161f781066b781
Harouna Tiambou Abdoul Wahabou <harounatiambou.pro@gmail.com>
2022-03-15 16:09:49 +0100

    Minimal routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Layout from './components/_Layout/Layout'
+import {Admin, AdminHome, AdminLogin, AdminRegister} from './pages/admin'
+import {User, Home, Login, Register} from './pages/user'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <Routes>
+            <Route path='/' element={<User />}>
+                <Route index path='' element={<Home />} />
+                <Route path=':id' element={<Home />} />
+                <Route path='login' element={<Login />} />
+                <Route path='register' element={<Register />} />
+            </Route>                
+            <Route path='admin' element={<Admin />}>
+                <Route index path='' element={<AdminLogin />} />
+                <Route path=':id' element={<AdminHome />} />
+                <Route path='register' element={<AdminRegister />} />
+            </Route>       
+        </Routes>
+    </div>
   )
 }
 
---
+++ src/pages/admin/index.jsx
@@ -0,0 +1,4 @@
+export {default as Admin} from './Admin'
+export {default as AdminHome} from './home/Home'
+export {default as AdminLogin} from './login/Login'
+export {default as AdminRegister} from './register/Register'
