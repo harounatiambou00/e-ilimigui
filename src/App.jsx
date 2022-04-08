82da50c9a0c35cc1db4cdb50f9fb604ba6d16e80
Harouna Tiambou Abdoul Wahabou <harounatiambou.pro@gmail.com>
2022-04-08 01:50:25 +0100

    Rethinking the files&Folders structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom'
 
 import "aos/dist/aos.css";
 
-import Layout from './components/_Layout/Layout'
 import {Admin, AdminHome, AdminLogin, AdminRegister} from './pages/admin'
 import {User, Home, SignIn, SignUp} from './pages/user'
 
