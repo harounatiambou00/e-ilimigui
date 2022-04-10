d2c3b04c04c674213dde662e45c749c70b8f04e1
Harouna Tiambou Abdoul Wahabou <harounatiambou.pro@gmail.com>
2022-04-10 14:53:03 +0100

    First step server side validation, to check if the email or the phone number already exists.

--- src/pages/user/sign-up/FirstStep/FirstStep.jsx
+++ src/pages/user/sign-up/FirstStep/FirstStep.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import useStyles from './styles';
 
-import { Divider, Grid, Button, InputLabel, OutlinedInput, FormControl, InputAdornment, FilledInput } from '@mui/material';
+import { Divider, Grid, Button, InputLabel, FormControl, InputAdornment, FilledInput, Alert } from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
-import { Email } from '@mui/icons-material';
+import { Email, Phone } from '@mui/icons-material';
+import { LoadingButton } from '@mui/lab';
 
-import Logo from '../../../../assets/images/Logo.png'
+import Logo from '../../../../assets/images/Logo.png';
+
+import SecondStep from '../SecondStep/SecondStep'
+
+
+import Aos from 'aos';
 
 const FirstStep = () => {
+
+  useEffect(() => {
+    Aos.init({duration : 1000})
+  }, [])
 
   const classes = useStyles();
 
@@ -24,10 +34,50 @@ const FirstStep = () => {
 
   const [isSellerAccount, setIsSellerAccount] = useState(false);
   const [signUpWithEmail, setSignUpWithEmail] = useState(false);
+  const [emailAlreadyExists, setEmailAlreadyExists] = useState(false);
+  const [phoneNumberAlreadyExists, setPhoneNumberAlreadyExists] = useState(false);
+  const [continueButtonIsLoading, setContinueButtonIsLoading] = useState(false);
+
+  const handleContinue = async() => {
+    setContinueButtonIsLoading(true);
+    if(isSellerAccount){
+      let response = await fetch("https://localhost:8000/api/sellers/all", {
+        method : "GET",
+      })
+      let content = await response.json();
+
+      let sellers = content.data; 
+
+      if(signUpWithEmail){
+        let sellersEmails = sellers.map(seller => seller.email);
+        sellersEmails.includes(values.email) ? setEmailAlreadyExists(true) : setEmailAlreadyExists(false);
+      }else{
+        let sellersPhoneNumbers = sellers.map(seller => seller.phoneNumber);
+        sellersPhoneNumbers.includes(values.phoneNumber) ? setPhoneNumberAlreadyExists(true) : setPhoneNumberAlreadyExists(false);
+      }
+    }
+    else{
+      let response = await fetch("https://localhost:8000/api/clients/all", {
+        method : "GET",
+      })
+      let content = await response.json();
+
+      let clients = content.data; 
+
+      if(signUpWithEmail){
+        let clientsEmails = clients.map(client => client.email);
+        clientsEmails.includes(values.email) ? setEmailAlreadyExists(true) : setEmailAlreadyExists(false);
+      }else{
+        let clientsPhoneNumbers = clients.map(client => client.phoneNumber);
+        clientsPhoneNumbers.includes(values.phoneNumber) ? setPhoneNumberAlreadyExists(true) : setPhoneNumberAlreadyExists(false);
+      }
+    }
+    window.setTimeout(() => setContinueButtonIsLoading(false), 1000)
+  }
 
   return (
     <div className={classes.first_step_page}>
-      <div className={classes.first_step_page_container}>
+      <div className={classes.first_step_page_container} data-aos="zoom-in">
         <div className={classes.first_step_page_header}>
           <img 
             src={Logo}
@@ -39,6 +89,7 @@ const FirstStep = () => {
         <Divider></Divider>
         <div className={classes.first_step_page_body}>
           <h4 className={classes.title}>Bienvenue sur E-ILIMI</h4>
+          <form>
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <h5 className={classes.user_type_choice_title}>Quel type de compte voulez-vous créer ?</h5>
@@ -71,19 +122,38 @@ const FirstStep = () => {
                   id="outlined-adornment-for-phone-number-or-email"
                   value={signUpWithEmail ? values.email : values.phoneNumber}
                   onChange={signUpWithEmail ? handleChange('email') : handleChange('phoneNumber')}
-                  startAdornment={signUpWithEmail ? '' : <InputAdornment position="start" sx={{ fontWeight: 'bold', pt : 0.5, ml : 2}}>+227</InputAdornment>}
+                  startAdornment={signUpWithEmail ? <InputAdornment position="start" sx={{ fontWeight: 'bold', pt : 0.5}}></InputAdornment> : <InputAdornment position="start" sx={{ fontWeight: 'bold', pt : 0.5, ml : 2}}>+227</InputAdornment>}
                   label="Amount"
+                  inputProps={signUpWithEmail ? {pattern : "[a-zA-Z_]+@[a-zA-z]+.[a-zA-Z_]{2,}"} : {inputMode : 'numeric', pattern : "[0-9]{8,8}", maxlength : 8}}
                 />
+                <Alert severity="error" sx={signUpWithEmail && emailAlreadyExists ? {mt : 1, mb : 1} : {display : 'none'}}>
+                  Cet adresse Email existe déja
+                </Alert>
+                <Alert severity="error" sx={!signUpWithEmail && phoneNumberAlreadyExists ? {mt : 1, mb : 1} : {display : 'none'}}>
+                  Ce numéro existe déja.
+                </Alert>
                 <small className={classes.send_message_notif}>
                   Nous vous enverrons un {signUpWithEmail ? "mail" : "SMS"} pour verifier que {signUpWithEmail ? "cette adresse email" : "ce numéro de telephone"} vous appartient.
-                  <strong>NB : </strong>Ce mail ou ce message vous couteras rien.<a href="#">Politique de confidentialité</a>.
+                  <strong>NB : </strong>Ce mail ou cet SMS vous couteras rien.<a href="#">Politique de confidentialité</a>.
                 </small>
               </FormControl>
             </Grid>
             <Grid item xs={12}>
-              <Button variant='contained' sx={{width: '100%', fontWeight: 'bold'}}>
+              <Button 
+                variant='contained'
+                sx={continueButtonIsLoading ? {display: 'none'} : {width: '100%', fontWeight: 'bold'}}
+                onClick={handleContinue}
+              >
                 Continuer
               </Button>
+              <LoadingButton 
+                variant='contained'
+                loading
+                sx={!continueButtonIsLoading ? {display: 'none'} : {width: '100%', fontWeight: 'bold'}}
+                onClick={handleContinue}
+              >
+                Continuer
+              </LoadingButton>
               <small className={classes.go_to_sign_in_page}>
                 Vous avez déja un compte ?&nbsp; <a href="#">Conectez-vous</a>
               </small>
@@ -93,13 +163,14 @@ const FirstStep = () => {
               <Button 
                 variant='outlined' 
                 sx={{width: '100%', fontWeight: 'bold', mt : 2, color : "#064663"}} 
-                startIcon={<Email />}
+                startIcon={signUpWithEmail ? <Phone /> : <Email />}
                 onClick={() => setSignUpWithEmail(!signUpWithEmail)}
                 >
-                Utiliser votre adresse Email
+                {signUpWithEmail ? "Utiliser votre numéro de téléphone" : "Utiliser votre adresse email"}
               </Button>
             </Grid>
           </Grid>
+          </form>
         </div>
       </div>
     </div>
